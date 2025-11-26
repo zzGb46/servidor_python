b7c92394543e404aeaae8f270f344f52192f9c07
zzGb46 <gabrielmatheussousa@hotmail.com>
2025-11-26 15:39:32 -0300

    continuacao

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+
+from flask_wtf import FlaskForm
+# wtforms é o motor principal quando se cria formularios com Flask
+from wtforms import  StringField, PasswordField, SubmitField
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'abc123'
+
+class RegisterForm(FlaskForm):
+   first_name= StringField('primeiro nome')
+   last_name = StringField('Sobrenome')
+   email = StringField('E-mail')
+   password = PasswordField('Senha')
+   confirm =  PasswordField('Confirme a senha')
+   submit = SubmitField('CADASTRAR')
+
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form= form)
+   
+
+@app.route('/exemplo')
+def exemplo():
+   session['nome'] = 'gabriel'
+   return f'o usuario é {session['nome']}'
 
 # definindo uma rota básica que responde a requisições GET
 @app.route("/")
@@ -29,7 +53,7 @@ def submit():
 
 @app.route('/template')
 def template():
-   return render_template('index.html')
+   return render_template('index.html',  name= 'gabriel')
 
 
 if __name__ == '__main__':
